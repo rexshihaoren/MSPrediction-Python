caa522560f347c3490fdf748235804420498c106
rexshihaoren <renshihao@gmail.com>
2014-05-22 14:58:31 -0700

    minor changes

--- pseudo2.py
+++ pseudo2.py
@@ -171,7 +171,6 @@ def plot(y_pred, y_true, plot_type):
 # 3. Meta-functions
 # You can then wrap the different stuff above with scripts that actually ask you for the kind of classifier you wnat, if you want to compare them, etc... But it's a piece of cake if you have buit the other bricks before !
 
-
 def simpleTest():
     data_path = '../MSPrediction-R/Data Scripts/data/predData.h5'
     obj = 'fam2'
@@ -219,88 +218,5 @@ def pred_prep(data_path, obj, target):
     X = newdataset.view((np.float64, len(newdataset.dtype.names)))
     y = y.view((np.float64, 1))
     return X, y, featureNames
-
-
-def compare_clf(X, y, clfs, obj, metric = 'roc'):
-    '''Compare classifiers with mean roc_auc'''
-    fig = pl.figure(figsize=(8,6),dpi=150)
-    rs0 = random.randint(1,1000)
-    # cv = KFold(len(y), n_folds = 10, shuffle = True)
-    # cv = StratifiedKFold(y, n_folds = 10)
-    for clfName in clfs.keys():
-        clf = clfs[clfName]
-        # y_pred, y_true = run_clf(clf, X, y, clfName, rs = rs0)
-        print clfName
-        y_pred, y_true = testAlgo(clf, X, y, clfName)
-        mean_fpr, mean_tpr, mean_auc = plot_unit_prep(y_pred, y_true, metric)
-        # print("Area under the ROC curve : %f" % mean_auc)
-        # Plot ROC curve
-        # plot(y_pred,y_true, plot_type = metric, no_fold = True)
-        pl.plot(mean_fpr, mean_tpr, lw=3, label = clfName + ' (area = %0.2f)' %mean_auc)
-    pl.plot([0, 1], [0, 1], 'k--')
-    pl.xlim([0.0, 1.0])
-    pl.ylim([0.0, 1.0])
-    pl.xlabel('False Positive Rate',fontsize=30)
-    pl.ylabel('True Positive Rate',fontsize=30)
-    pl.title('Receiver Operating Characteristic',fontsize=25)
-    pl.legend(loc='lower right')
-    pl.tight_layout()
-    # fig.savefig('plots/'+obj+'/'+'clf_comparison_'+ metric +'.pdf')
-    pl.show()
-
-if __name__ == "__main__":
-    debugging = True
-    '''Some basic setup for prediction'''
-    ####### This part can be modified to fulfill different needs ######
-    data_path = '../MSPrediction-R/Data Scripts/data/predData.h5'
-    obj = 'fam2'
-    target = 'EnjoyLife'
-    ########## Can use raw_input instead as well#######################
-    X, y, featureNames = pred_prep(data_path, obj, target)
-    num_features = X.shape[1]
-    classifiers = {"LogisticRegression": LogisticRegression(), 
-                    "KNN": KNeighborsClassifier(),
-                    "Bayes": BernoulliNB(),
-                    "SVM": SVC(probability = True),
-                    "RandomForest": RandomForestClassifier(),
-                    "LinearRegression": LinearRegression()
-                    }
-    # dictionaries of different classifiers, these can be eyeballed from my parameter sweeping curve
-    RFParamDist = {"n_estimators": range(25,45),
-                  "max_features": range(1, num_features + 1),
-                  "min_samples_split": range(1, 30),
-                  "min_samples_leaf": range(1, 10),
-                  "bootstrap": [True, False],
-                  "criterion": ["gini", "entropy"]}
-    logRegParamDist = {}
-    KNNParamDist = {}
-    BayesParamDist = {}
-    SVMParamDist = {}
-    LinRegParamDist = {}
-    # a dictionary storing the param_dist for different classifiers
-    param_dist_dict = {"LogisticRegression": logRegParamDist,
-                    "KNN":KNNParamDist,
-                    "Bayes":BayesParamDist,
-                    "SVM":SVMParamDist,
-                    "RandomForest":RFParamDist,
-                    "LinearRegression":LinRegParamDist
-                    }
-                    
-    com_clf = raw_input("Compare classifiers? (Y/N) ")
-    if com_clf == "Y":
-        compare_clf(X, y, classifiers, obj)
-    else:
-        clf, clfName = choose_clf(classifiers)
-        param_sweep = raw_input("Parameter Sweeping? (Y/N) ")
-        if param_sweep == "Y" or param_sweep == "y":
-            param, param_dist, metric = param_sweep_select(clf)
-            param_sweeping(clf, obj, X, y, param_dist, metric, param, clfName)
-        else:
-            print ("Your only choice now is to plot ROC and PR curves for "+clfName+" classifier")
-            # Asking whether to optimize
-            opt = raw_input("Optimization? (Y/N)")
-            opt = (opt== "Y" or opt == "y")
-            param_dist = param_dist_dict[clfName]
-            # clf_plot(clf, X, y, clfName, obj, param_dist, opt)
 
 
